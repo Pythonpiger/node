daca1eccec220e10ffe3282015f8f9ffe6ed0df1
Pythonpiger <617238338@qq.com>
2018-01-24 19:07:18 +0800

    1/24

--- test/server1.js
+++ test/server1.js
@@ -4,4 +4,4 @@ var server = http.createServer(function(req,res){
 	res.writeHead(200,{"Content-type":"text/html;charset=UTF-8"});
 	res.end("LZ我要和你网恋！")
 });
-server.listen(8088,"127.0.0.1")
+server.listen(8088,"127.0.0.1");
